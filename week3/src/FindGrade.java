0dc00f754728ae0c3e02070d6e81ae5eaf909b4c
debugit0 <oguzhank.34b@gmail.com>
2025-03-07 11:59:33 +0300

    week3

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -10,9 +10,24 @@ public class FindGrade{
         // 70 - 79 -> C
         // 60 - 69 -> D
         // 0 - 59 -> F
+        if (args.length >= 1) {
+            int score = Integer.parseInt(args[0]);
+            if (score >= 90 && score <= 100) {
+                System.out.println("Grade: A");
+            } else if (score >= 80 && score <89) {
+                System.out.println("Grade: B");
+            } else if (score >= 70 && score <79) {
+                System.out.println("Grade: C");
+            } else if (score >= 60 && score <79) {
+                System.out.println("Grade: D");
+            } else if (score >=0 && score <59) {
+                System.out.println("Grade: E");
+            }else {
+                System.out.println("Invalid Grade");
+            }
 
 
-
+        }
 
 
     }
